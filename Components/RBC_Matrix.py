8620d710f9ac38cc3c06b664c996266a28213e80
Liav <liavbapp@gmail.com>
2020-11-24 23:23:50 +0200

    more changes to rbc_matrix

--- Components/RBC_Matrix.py
+++ Components/RBC_Matrix.py
@@ -2,36 +2,44 @@ import networkx as nx
 import numpy as np
 
 
-def rbc(g: nx.DiGraph, R, T):
+def rbc(g, R, T):
     rbc_arr = np.full(shape=(g.number_of_nodes(), 1), fill_value=0, dtype=np.float)
     nodes_map = {k: v for v, k in enumerate(list(g.nodes()))}
-    for s in g.nodes():
-        for t in g.nodes():
-            rbc_source_target(g.number_of_nodes(), R, T, rbc_arr, nodes_map[s], nodes_map[t])
+    s_mapping = [nodes_map[node] for node in g.nodes()]
+    t_mapping = s_mapping
+    num_nodes = g.number_of_nodes()
+
+    for s in s_mapping:
+        for t in t_mapping:
+            delta_arr = np.full(shape=(num_nodes, 1), fill_value=0, dtype=np.float)
+            delta_arr[s] = T(s, t)
+            updated_delta = accumulate_delta(delta_arr, s, R(s, t))
+            rbc_arr += updated_delta
+
     return rbc_arr, nodes_map
 
 
-def rbc_source_target(num_nodes, R, T, rbc_arr, s, t):
-    delta_arr = init_delta_arr(num_nodes, T=T, source=s, target=t)
-    updated_delta = accumulate_delta(delta_arr, s, t, R)
-    rbc_arr += updated_delta
+# def rbc_source_target(num_nodes, R, T, rbc_arr, s, t):
+#     delta_arr = init_delta_arr(num_nodes, T=T, source=s, target=t)
+#     updated_delta = accumulate_delta(delta_arr, s, t, R)
+#     rbc_arr += updated_delta
+
+#
+# def init_delta_arr(num_nodes, T, source, target):
+#     delta_arr = np.full(shape=(num_nodes, 1), fill_value=0, dtype=np.float)
+#     delta_arr[source] = T(source, target)
+#
+#     return delta_arr
 
 
-def init_delta_arr(num_nodes, T, source, target):
-    delta_arr = np.full(shape=(num_nodes, 1), fill_value=0, dtype=np.float)
-    delta_arr[source] = T(source, target)
-
-    return delta_arr
-
-
-def accumulate_delta(delta_arr, src, target, R):
-    predecessor_prob_matrix = R(src, target)
+def accumulate_delta(delta_arr, src, predecessor_prob_matrix):
     predecessors = {src}
     while len(predecessors) > 0:
         delta_arr += np.matmul(predecessor_prob_matrix, delta_arr)
         new_predecessors = set()
         for old_predecessor in predecessors:
-            new_predecessors = new_predecessors.union(set(np.nonzero(predecessor_prob_matrix[:, ])[0]))
+            old_successors = np.nonzero(predecessor_prob_matrix[:, ])[0]
+            new_predecessors = new_predecessors.union(set(old_successors))
             predecessor_prob_matrix[:, old_predecessor] = 0
         predecessors = new_predecessors
     return delta_arr
@@ -57,14 +65,16 @@ def to_tuple_edge_paths(all_shortest_path):
     return all_edges_tuple_paths
 
 
-def get_betweenness_policy_dict(g, nodes_mapping):
-    betweenness_dict = {}
+def get_betweenness_policy_matrix(g, nodes_mapping):
+
+    num_nodes = g.number_of_nodes()
+    betweenness_matrix = np.full(shape=(num_nodes, num_nodes, num_nodes, num_nodes), fill_value=0, dtype=float)
     for s in g.nodes():
         for t in g.nodes():
             edges_probabilities = get_edges_probabilities(g, s, t, nodes_mapping)
-            betweenness_dict.update({(nodes_mapping[s], nodes_mapping[t]): edges_probabilities})
+            betweenness_matrix[nodes_mapping[s]][nodes_mapping[t]] = edges_probabilities
 
-    return betweenness_dict
+    return betweenness_matrix
 
 
 def get_edges_probabilities(g, src, target, nodes_mapping):
@@ -85,9 +95,7 @@ def get_edges_probabilities(g, src, target, nodes_mapping):
 
 if __name__ == '__main__':
     edges = {('v1', 'v2'), ('v2', 'v3'), ('v2', 'v4'), ('v3', 'v4')}
-    g = nx.DiGraph(edges)
-    nodes_mapping = {k: v for v, k in enumerate(list(g.nodes()))}
-    policy = get_betweenness_policy_dict(g, nodes_mapping)
-    source = 'v1'
-    dest = 'v4'
-    res = rbc(g, lambda s, t: policy[(s, t)], lambda s, t: 100)
+    graph = nx.DiGraph(edges)
+    nodes_mapping = {k: v for v, k in enumerate(list(graph.nodes()))}
+    policy = get_betweenness_policy_matrix(graph, nodes_mapping)
+    res = rbc(graph, lambda s, t: policy[s][t], lambda s, t: 100)
