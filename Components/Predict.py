3a492518ab97427160e25a2bb0aa9e7e6025c19f
Liav <liavbapp@gmail.com>
2020-12-18 01:00:44 +0200

    first impl of Predict

--- Components/Predict.py
+++ Components/Predict.py
@@ -6,11 +6,53 @@ from Components import Policy
 import numpy as np
 
 
+def predict_degree(input_features, target_degree):
+    dtype = torch.float
+    device = torch.device("cuda:0")
 
-def predict_degree(input_features, traffic_matrix, target_degree):
-    # initial_weights = torch.rand(size=(8, 1))
-    # output = torch.mm(input_features, initial_weights)
-    # output.requires_grad = True
+    # Create Tensors to hold input and outputs.
+    # By default, requires_grad=False, which indicates that we do not need to
+    # compute gradients with respect to these Tensors during the backward pass.
+    x = np.array(input_features, dtype=float)
+    y = torch.tensor(target_degree, device=device, dtype=dtype)
+
+    # Create random Tensors for weights. we need len(input features) weights.
+    weights_lst = [[torch.randn((), device=device, dtype=dtype, requires_grad=True)] for k in range(input_features.shape[1])]
+
+    weights = np.array(weights_lst)
+
+    learning_rate = 0.01
+
+    epochs = 1000
+    for e in range(epochs):
+        y_pred = np.matmul(x, weights)[0][0]
+
+        # Compute and print loss using operations on Tensors.
+        # Now loss is a Tensor of shape (1,)
+        # loss.item() gets the scalar value held in the loss.
+        loss = nn.MSELoss()
+        d_loss = loss(y_pred, y)
+        if e % 100 == 99:
+            print(e, d_loss.item())
+
+        # Use autograd to compute the backward pass. This call will compute the
+        # gradient of loss with respect to all Tensors with requires_grad=True.
+        # After this call a.grad, b.grad. c.grad and d.grad .... will be Tensors holding
+        # the gradient of the loss with respect to a, b, c, d .... respectively.
+        d_loss.backward()
+
+        # Manually update weights using gradient descent. Wrap in torch.no_grad()
+        # because weights have requires_grad=True, but we don't need to track this
+        # in autograd.
+        with torch.no_grad():
+            # weights -= learning_rate * grads
+            for weight in weights:
+                weight[0] -= learning_rate * weight[0].grad
+                # zero the grad manually
+                weight[0].grad = None
+
+    print(f'degree is: {y_pred}')
+
 
 
 if __name__ == '__main__':
@@ -18,66 +60,4 @@ if __name__ == '__main__':
     # edge_lst = [('v0', 'v1')]
     # g = nx.Graph(edge_lst).to_directed()
     # t_tensor = degree_policy.get_t_tensor(g)
-    # predict_degree(torch.tensor([[0, 0, 0, 0, 0.5, 0.5, 0.5, 0.5]]), traffic_matrix=t_tensor,
-    #                target_degree=torch.tensor([[1.0]]))
-
-    dtype = torch.float
-    device = torch.device("cuda:0")
-
-    # Create Tensors to hold input and outputs.
-    # By default, requires_grad=False, which indicates that we do not need to
-    # compute gradients with respect to these Tensors during the backward pass.
-    x = np.array([0, 0, 0, 0, 0.5, 0.5, 0.5, 0.5], dtype=float)
-    y = torch.tensor(1.0, device=device, dtype=dtype)
-
-    # Create random Tensors for weights. we need 8 weights: a,b,c,d,e,f,g,h
-    a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    e = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    f = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    g = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    h = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    weights = np.array([[a], [b], [c], [d], [e], [f], [g], [h]])
-
-    learning_rate = 1e-6
-
-    for t in range(2000):
-        # Forward pass: compute predicted y using operations on Tensors.
-        y_pred = np.matmul(x, weights)[0]
-
-        # Compute and print loss using operations on Tensors.
-        # Now loss is a Tensor of shape (1,)
-        # loss.item() gets the scalar value held in the loss.
-        loss_func = nn.MSELoss()
-        delta_loss = loss_func(y_pred, y)
-        if t % 100 == 99:
-            print(t, delta_loss.item())
-
-        # Use autograd to compute the backward pass. This call will compute the
-        # gradient of loss with respect to all Tensors with requires_grad=True.
-        # After this call a.grad, b.grad. c.grad and d.grad will be Tensors holding
-        # the gradient of the loss with respect to a, b, c, d respectively.
-        delta_loss.backward()
-
-        # Manually update weights using gradient descent. Wrap in torch.no_grad()
-        # because weights have requires_grad=True, but we don't need to track this
-        # in autograd.
-        grads = np.full(size=(len(x), 1), dtype=float, fill_value=0.0)
-        for i in range(0, weights.size):
-            grads[i][0] = weights[i][0].grad
-
-        with torch.no_grad():
-            weights -= learning_rate * grads
-
-            # Manually zero the gradients after updating weights
-            for i in range(0, weights.size):
-                weights[i][0].grad = None
-
-    print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
-
-
-
-
-
+    predict_degree(np.array([[0, 0, 0, 0, 0.5, 0.5, 0.5, 0.5]], dtype=float), target_degree=1)
