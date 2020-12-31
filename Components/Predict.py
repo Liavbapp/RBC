867060760a33fdd5dd55ea6a095fcc28fc5ab680
Liav <liavbapp@gmail.com>
2020-12-31 13:11:07 +0200

    degree with fixed matrix. automatically building adj matrix of graphs

--- Components/Predict.py
+++ Components/Predict.py
@@ -1,3 +1,5 @@
+import datetime
+
 import torch
 import math
 from torch import nn
@@ -27,31 +29,30 @@ class DegreePrediction(torch.nn.Module):
 
     def forward(self, x, r_zeros, r_const):
         layer2 = x * self.weights_t
-        weights_r_comb = self.weights_r * r_zeros + r_const
-        l2_mult = layer2.view(self.n_nodes_pow2, 1) * weights_r_comb.view(self.n_nodes_pow2, self.n_nodes_pow2)
-        l2_mult = l2_mult.view(self.num_nodes, self.num_nodes, self.num_nodes, self.num_nodes)
-        y_pred = torch.sum(l2_mult, 1).sum(0).sum(1)
+        weights_r_comb = torch.mul(self.weights_r, r_zeros) + r_const
+        layer3 = torch.mul(layer2.view(self.n_nodes_pow2, 1), weights_r_comb.view(self.n_nodes_pow2, self.n_nodes_pow2))
+        layer3 = layer3.view(self.num_nodes, self.num_nodes, self.num_nodes, self.num_nodes)
+        y_pred = torch.sum(layer3, 1).sum(0).sum(1)
         return y_pred
 
 
-def predit_degree_custom_model(adj_mat, y):
+def predict_degree_custom_model(adj_mat, y):
     model = DegreePrediction(adj_mat)
     criterion = torch.nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
     zero_mat, const_mat = get_fixed_mat(adj_mat)
 
-    for t in range(4000):
+    start_time = datetime.datetime.now()
+    for t in range(250):
         y_pred = model(adj_mat, zero_mat, const_mat)
         loss = criterion(y_pred, y)
-        print(t, loss.item())
+        if t % 20 == 0:
+            print(t, loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        # with torch.no_grad():
-        #     model.weights_t.copy_(model.weights_t.data.clamp(min=0))
-        #     model.weights_r.copy_(model.weights_r.data.clamp(min=0))
 
-    print(f'prediction : {y_pred}, target:{y}')
+    print(f'run time: {datetime.datetime.now() - start_time} , prediction : {y_pred}, target:{y}')
     model_t = model.weights_t * adj_mat
     model_r = model.weights_r * zero_mat + const_mat
     return model_t, model_r
@@ -83,14 +84,37 @@ if __name__ == '__main__':
     # rbc_pred = RBC.rbc(g, r_model, t_model)
     # print(rbc_pred)
 
+    # degree_policy = Policy.DegreePolicy()
+    # edge_lst = [('v0', 'v1'), ('v1', 'v2'), ('v0', 'v2'), ('v2', 'v3')]
+    # g = nx.Graph(edge_lst).to_directed()
+    # t_tensor = degree_policy.get_t_tensor(g)
+    # t_model, r_model = predit_degree_custom_model(
+    #     torch.tensor([[0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 0, 1], [0, 0, 1, 0]], dtype=DTYPE,
+    #                  device=DEVICE, requires_grad=True), torch.tensor([2, 2, 3, 1], device=DEVICE, dtype=DTYPE))
+    # t_model = t_model.to(device=torch.device("cpu"))
+    # r_model = r_model.to(device=torch.device("cpu"))
+    # rbc_pred = RBC.rbc(g, r_model, t_model)
+    # print(rbc_pred)
+
+    # degree_policy = Policy.DegreePolicy()
+    # edge_lst = [('v0', 'v1'), ('v1', 'v2'), ('v0', 'v2'), ('v2', 'v3')]
+    # g = nx.Graph(edge_lst)
+    # adj_matrix = torch.from_numpy(nx.to_numpy_matrix(g)).to(dtype=DTYPE, device=DEVICE)
+    # target_matrix = torch.tensor(list(map(float,dict(nx.degree(g)).values())), device=DEVICE, dtype=DTYPE)
+    # g = g.to_directed()
+    # t_model, r_model = predit_degree_custom_model(adj_matrix, target_matrix)
+    # t_model = t_model.to(device=torch.device("cpu"))
+    # r_model = r_model.to(device=torch.device("cpu"))
+    # rbc_pred = RBC.rbc(g, r_model, t_model)
+    # print(rbc_pred)
+
     degree_policy = Policy.DegreePolicy()
-    edge_lst = [('v0', 'v1'), ('v1', 'v2'), ('v0', 'v2'), ('v2', 'v3')]
-    g = nx.Graph(edge_lst).to_directed()
-    t_tensor = degree_policy.get_t_tensor(g)
-    t_model, r_model = predit_degree_custom_model(
-        torch.tensor([[0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 0, 1], [0, 0, 1, 0]], dtype=DTYPE,
-                     device=DEVICE, requires_grad=True), torch.tensor([2, 2, 3, 1], device=DEVICE, dtype=DTYPE))
-    t_model = t_model.to(device=torch.device("cpu"))
-    r_model = r_model.to(device=torch.device("cpu"))
-    rbc_pred = RBC.rbc(g, r_model, t_model)
-    print(rbc_pred)
+    g = nx.watts_strogatz_graph(n=90, k=6, p=0.5)
+    adj_matrix = torch.from_numpy(nx.to_numpy_matrix(g)).to(dtype=DTYPE, device=DEVICE)
+    target_matrix = torch.tensor(list(map(float, dict(nx.degree(g)).values())), device=DEVICE, dtype=DTYPE)
+    g = g.to_directed()
+    t_model, r_model = predict_degree_custom_model(adj_matrix, target_matrix)
+    # t_model = t_model.to(device=torch.device("cpu"))
+    # r_model = r_model.to(device=torch.device("cpu"))
+    # rbc_pred = RBC.rbc(g, r_model, t_model)
+    # print(rbc_pred)
