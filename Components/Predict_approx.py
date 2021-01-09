ebb2c2de0e719b5692663ac76396d8a4571e9c22
Liav <liavbapp@gmail.com>
2021-01-09 15:21:09 +0200

    with eigne approximation learning both R and T

--- Components/Predict_approx.py
+++ Components/Predict_approx.py
@@ -1,12 +1,9 @@
 import datetime
-import math
-
 import torch
-from torch import nn
 import networkx as nx
 from Components import Policy
-import numpy as np
 import Components.RBC as RBC
+import numpy as np
 
 DTYPE = torch.float
 DEVICE = torch.device("cuda:0")
@@ -21,31 +18,26 @@ class DegreePrediction(torch.nn.Module):
         self.n_nodes_pow2 = pow(self.num_nodes, 2)
         self.n_nodes_pow3 = pow(self.num_nodes, 3)
         self.weights_t = torch.nn.Parameter(
-            torch.randn(self.num_nodes, self.num_nodes, requires_grad=True, device=device, dtype=dtype))
+            torch.rand(self.num_nodes, self.num_nodes, requires_grad=True, device=device, dtype=dtype))
+        self.weights_r = torch.nn.Parameter(torch.rand(self.num_nodes, self.num_nodes, self.num_nodes, self.num_nodes,
+                                                       requires_grad=True, device=device, dtype=dtype))
 
-        self.weights_r = torch.nn.Parameter(
-            torch.randn(self.num_nodes, self.num_nodes, self.num_nodes, self.num_nodes,
-                        requires_grad=True, device=device, dtype=dtype))
+        self.self_eigenvalue = torch.tensor([[1.0, 0.0]], device=DEVICE, dtype=DTYPE)
 
     def forward(self, x, r_zeros, r_const):
         layer2 = (x * self.weights_t).view(self.num_nodes, self.num_nodes, 1, 1) * r_const
         weights_r_comb = torch.mul(self.weights_r, r_zeros) + r_const
-        all_delta_arrays = [self.accumulate_delta(s, weights_r_comb[s, t].clone().detach(), layer2[s, t, s, s]) for s in
+        all_delta_arrays = [self.accumulate_delta(s, weights_r_comb[s, t], layer2[s, t, s, s]) for s in
                             range(0, len(x)) for t in range(0, len(x))]
         rbc_arr = torch.sum(torch.stack(all_delta_arrays), dim=0)
-
-        # layer3 = torch.mul(layer2.view(self.n_nodes_pow2, 1), weights_r_comb.view(self.n_nodes_pow2, self.n_nodes_pow2))
-        # layer3 = layer3.view(self.num_nodes, self.num_nodes, self.num_nodes, self.num_nodes)
-        # y_pred = torch.sum(layer3, 1).sum(0).sum(1)
         return rbc_arr
 
     def accumulate_delta(self, src, predecessor_prob_matrix, T_val):
-        eigenvalues, eigenvectors = torch.eig(input=predecessor_prob_matrix, eigenvectors=True)
-        eigenvector = self.get_eigenvector_by_eigenvalue(eigenvalues, eigenvectors, torch.tensor([[1.0, 0.0]],
-                                                                                                 device=DEVICE,
-                                                                                                 dtype=DTYPE))
-        eigenvector = self.compute_eigenvector_values(src, eigenvector, T_val)
+
+        eigenvector2 = self.power_iteration(predecessor_prob_matrix)[1]
+        eigenvector = self.compute_eigenvector_values(src, eigenvector2, T_val)
         return eigenvector
+
 
     def compute_eigenvector_values(self, src, eigenvector, T_val):
         x = 1 / float(eigenvector[src])  # the ratio between 1 to the current value of eigenvector[src]
@@ -54,22 +46,44 @@ class DegreePrediction(torch.nn.Module):
 
         return n_eigenvector
 
-    def get_eigenvector_by_eigenvalue(self, eigenvalues, eigenvectors, eigenvalue):
-        comparison_sum = (eigenvalues == eigenvalue).sum(dim=1)  # pair-wise compare and sum how many elements equal
-        indicies = torch.nonzero(comparison_sum == eigenvalues.size(1))  # the equal indexes
-        eigenvalue_idx = int(indicies[0][0])  # extract the index
-        eigenvector = eigenvectors[:, eigenvalue_idx]  # extract the eigenvector col corresponding to eigenvalue index
-        return eigenvector
+
+
+
+    def eigenvalue(self, A, v):
+        Av = torch.mm(A, v)
+        return torch.dot(v.flatten(), Av.flatten())
+
+    def power_iteration(self, A, num_iter=100):
+        n, d = A.shape
+        v = (torch.ones(d) / np.sqrt(d)).to(device=DEVICE, dtype=DTYPE).view(d, 1)
+        ev = self.eigenvalue(A, v)
+
+        i = 0
+        while i < num_iter:
+            i += 1
+            Av = torch.mm(A, v)
+            v_new = Av / torch.linalg.norm(Av.flatten())
+
+            ev_new = self.eigenvalue(A, v_new)
+            if torch.abs(ev - ev_new) < 0.01:
+                break
+
+            v = v_new
+            ev = ev_new
+
+        return ev_new, v_new.flatten()
+
 
 
 def predict_degree_custom_model(graph, adj_mat, y):
-    model = DegreePrediction(adj_mat)
-    criterion = torch.nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
     zero_mat, const_mat = get_fixed_mat(adj_mat, graph)
 
+    model = DegreePrediction(adj_mat)
+    criterion = torch.nn.MSELoss(reduction='sum')
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
     start_time = datetime.datetime.now()
-    for t in range(100):
+    for t in range(150):
         y_pred = model(adj_mat, zero_mat, const_mat)
         loss = criterion(y_pred, y)
         if t % 20 == 0:
@@ -104,8 +118,9 @@ def get_fixed_mat(adj_mat, graph):
 
 if __name__ == '__main__':
     degree_policy = Policy.DegreePolicy()
-    edge_lst = [(0, 1), (1, 2), (0, 2)]
+    edge_lst = [(0, 1), (1, 2)]
     g = nx.Graph(edge_lst)
+    # g = nx.watts_strogatz_graph(n=10, k=3, p=0.5)
     adj_matrix = torch.from_numpy(nx.to_numpy_matrix(g)).to(dtype=DTYPE, device=DEVICE)
     target_matrix = torch.tensor(list(map(float, dict(nx.degree(g)).values())), device=DEVICE, dtype=DTYPE)
     g = g.to_directed()
